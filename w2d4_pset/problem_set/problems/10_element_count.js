e971699b0a5cbc86bdf5dd8e9246c7f797dede53
Mlkz <maho.mamulashvili@gmail.com>
2022-09-09 20:19:05 -0400

    element count in array

--- w2d4_pset/problem_set/problems/10_element_count.js
+++ w2d4_pset/problem_set/problems/10_element_count.js
@@ -9,7 +9,16 @@ elementCount(['c', 'a', 'c', 'a', 'b']); // => { "c": 2, "a": 2, "b": 1 }
 ***********************************************************************/
 
 function elementCount(array) {
+  let obj = {};
+  for (let i = 0; i < array.length; i++) {
+    if (obj[array[i]] === undefined){
+      obj[array[i]] = 1;
+    } else {
+      obj[array[i]] += 1
+    }
+  }
 
+  return obj;
 }
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = elementCount;
